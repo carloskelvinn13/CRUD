d546f606690c8c3b6d039ad32b7b992f61b439f5
carloskelvinn13 <kelvin132009@gmail.com>
2022-02-21 15:13:51 -0300

    BlogPessoal

---
+++ src/main/java/org/generation/blogPessoal2/BlogPessoal2Application.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class BlogPessoal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoal2Application.class,args);
+	}
+
+}
---
+++ src/test/java/org/generation/blogPessoal2/BlogPessoal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
